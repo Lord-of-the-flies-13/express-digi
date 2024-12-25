aefb1eec1b87be9c89945ec5199dc599932dbdd1
lord-of-the-flies-13 <rohancan045@gmail.com>
2024-12-25 16:47:53 +0530

    add changes

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+import 'dotenv/config'
 import express from 'express'
 
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
 let pokedata = []
 let nextId = 1
 
+// add a new pokemon
 app.post('/pokemon', (req, res) => {
     const {name, price} = req.body
     const newPoke = {id: nextId++, name, price}
@@ -16,12 +18,46 @@ app.post('/pokemon', (req, res) => {
     res.status(201).send(newPoke)
 })
 
+// get all pokemon
 app.get('/pokemon', (req,res) => {
     res.status(200).send(pokedata)
 })
 
+// get a pokemon with id
 app.get('/pokemon/:id', (req,res) => {
-    pokedata.find(t => t.id === req.params.id)
+    const poke = pokedata.find(t => t.id === parseInt(req.params.id))
+    if(!poke)
+    {
+        return res.status(404).send('Pokrmon not found')
+    }
+    res.status(200).send(poke)
+})
+
+// update pokemon
+app.put('/pokemon/:id', (req,res) => {
+    const poke = pokedata.find(t => t.id === parseInt(req.params.id))
+
+    if(!poke)
+    {
+        return res.status(404).send('Pokrmon not found')
+    }
+
+    const {name, price} = req.body
+    poke.name = name
+    poke.price = price
+    res.send(200).send(poke)
+})
+
+// delete pokemon
+app.delete('/pokemon/:id', (req,res) => {
+    const index = pokedata.findIndex(t => t.id === parseInt(req.params.id))
+
+    if(index===-1)
+    {
+        return res.status(404).send('pokemon not found')
+    }
+    pokedata.splice(index, 1)
+    return res.status(204).send('deleted')
 })
 
 app.listen(port, () => {
